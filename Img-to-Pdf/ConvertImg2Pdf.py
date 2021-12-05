729629f528663b78622bba46397c8cd7729fe175
Korra15 <mailayushi15@gmail.com>
2021-12-05 16:08:11 +0530

    Added img-to-pdf functionality

---
+++ Img-to-Pdf/ConvertImg2Pdf.py
@@ -0,0 +1,8 @@
+import img2pdf
+
+file = open("generatedPdf.pdf", "wb")
+imgPath = input("Please enter the path of the image to be converted: ")
+pdfData = img2pdf.convert(imgPath)
+file.write(pdfData)
+file.close()
+print("Pdf converted")
