b4401b2684ad59e22108f33a0745517f3a104fe9
Abir Pal <abirpal282002@gmail.com>
2021-12-28 00:42:03 +0530

    Fixing the bug in chatbot and index-to-pdf convertor

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 const navbar = document.getElementById('navbar')
-navbar.innerHTML="<nav><div class=\"logo\"><h6 id=\"logo\">Student Portal</h6></div><div class=\"toggle-btn\"><span class=\"bar\"></span><span class=\"bar\"></span><span class=\"bar\"></span></div><div class=\"nav-links\"><ul><li><a class=\"active\" href=\"./index.html\">Home</a></li><li><a href=\"./index.html\">Services</a></li><li><a href=\"./contact.html\">Contact</a></li><li><a href=\"./index.html\">About</a></li></ul></div></nav>"
+navbar.innerHTML = `<nav>
+<div class="logo">
+    <h6 id="logo">Student Portal</h6>
+</div>
+<div class="toggle-btn">
+<span class="bar"></span>
+<span class="bar"></span>
+<span class="bar"></span>
+</div>
+<div class="nav-links">
+<ul>
+    <li><a class="active" href="./index.html">Home</a></li>
+    <li><a href="./index.html">Services</a></li>
+    <li><a href="./contact.html">Contact</a></li>
+    <li><a href="./index.html">About</a></li>
+</ul>
+</div>
+</nav>`
 const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
 const navLinks = document.getElementsByClassName("nav-links")[0];
 
