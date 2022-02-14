95efe86a6905414a45a535485b600404cd992ab0
devika <devikavarshney0003@gmail.com>
2022-02-14 16:14:58 +0530

    corrected reset button functionality and UI design

--- Functionalities/Word-to-Pdf/script.js
+++ Functionalities/Word-to-Pdf/script.js
@@ -1,13 +1,13 @@
 var addfile = document.querySelector("#addfile");
 var pdf = document.querySelector(".pdf");
 var dwpdf = document.querySelector(".dwpdf");
-var reset = document.querySelector("#reset");
+var reset = document.querySelector(".reset");
 var addfilelab = document.querySelector("#addfilelab");
 
 var opt = {
   margin: 1,
   pagebreak: { mode: "avoid-all" },
-  filename: "myfile.pdf",
+  filename: "StudentPortal.pdf",
   image: { type: "jpeg", quality: 0.98 },
   html2canvas: { scale: 1 },
   jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
@@ -28,6 +28,7 @@ addfile.addEventListener("change", (e) => {
         .convertToHtml({ arrayBuffer: arrayBuffer })
         .then(function (resultObject) {
           pdf.innerHTML = resultObject.value;
+          pdf.style.color = "#222";
           console.log(resultObject.value);
         });
       console.timeEnd();
@@ -36,6 +37,8 @@ addfile.addEventListener("change", (e) => {
     pdf.style.display = "none";
     dwpdf.disabled = false;
     dwpdf.style.opacity = "1";
+    reset.disabled = false;
+    reset.style.opacity = "1";
     if (addfile.value != null) {
       addfilelab.innerHTML =
         '<i class="fa fa-check" aria-hidden="true" ></i> Uploaded .doc file';
@@ -50,10 +53,12 @@ function dwnldpdf() {
   setTimeout(() => {
     pdf.style.display = "none";
   }, 1);
-  dwpdf.classList.remove("dwpdf_hover"); 
+  dwpdf.classList.remove("dwpdf_hover");
   addfile.value = null;
-  dwpdf.disabled = true;  
+  dwpdf.disabled = true;
   dwpdf.style.opacity = "0.5";
+  reset.disabled = true;
+  reset.style.opacity = "0.5";
   addfilelab.innerHTML = '<i class="fa fa-cloud-upload"></i> Upload .doc file';
   addfilelab.style.pointerEvents = "auto";
 }
@@ -61,16 +66,24 @@ function dwnldpdf() {
 dwpdf.addEventListener("click", dwnldpdf);
 
 dwpdf.addEventListener("mouseover", () => {
-    dwpdf.classList.add("dwpdf_hover");
-  });
-  dwpdf.addEventListener("mouseout", () => {
-    dwpdf.classList.remove("dwpdf_hover");
-  });
+  dwpdf.classList.add("dwpdf_hover");
+});
+dwpdf.addEventListener("mouseout", () => {
+  dwpdf.classList.remove("dwpdf_hover");
+});
+reset.addEventListener("mouseover", () => {
+  reset.classList.add("reset_hover");
+});
+reset.addEventListener("mouseout", () => {
+  reset.classList.remove("reset_hover");
+});
 
 reset.addEventListener("click", () => {
   addfile.value = null;
-  dwpdf.disabled = true;  
+  dwpdf.disabled = true;
   dwpdf.style.opacity = "0.5";
+  reset.disabled = true;
+  reset.style.opacity = "0.5";
   addfilelab.innerHTML = '<i class="fa fa-cloud-upload"></i> Upload .doc file';
   addfilelab.style.pointerEvents = "auto";
 });
