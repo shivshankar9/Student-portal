38b90d36c13b0f22d5ef89dd22681cc2b527269a
devika <devikavarshney0003@gmail.com>
2022-02-17 17:16:22 +0530

    corrected navbar order

--- Functionalities/Text-to-pdf/main.js
+++ Functionalities/Text-to-pdf/main.js
@@ -15,9 +15,9 @@ navbar.innerHTML = `<nav>
 <div class="nav-links">
 <ul>
     <li><a class="active" href="../../index.html">Home</a></li>
+    <li><a href="../../index.html#about-section">About</a></li>
     <li><a href="../../index.html">Services</a></li>
     <li><a href="../../contact.html">Contact</a></li>
-    <li><a href="../../index.html#about-section">About</a></li>
 </ul>
 </div>
 </nav>`;
