be601d622ecea412b4b28354ba538c2a4a7dc066
devika <devikavarshney0003@gmail.com>
2022-02-24 09:54:30 +0530

    navbar activity during scrolling corrected

--- js/script.js
+++ js/script.js
@@ -50,3 +50,15 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+// scrolling navbar activity
+const li = document.querySelectorAll(".btn");
+const sec = document.querySelectorAll("section");
+function activeMenu(){
+  let len = sec.length;
+  while(--len && window.scrollY + 70 < sec[len].offsetTop){}
+  li.forEach(ltx => ltx.classList.remove("active"));
+  li[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll",activeMenu);
